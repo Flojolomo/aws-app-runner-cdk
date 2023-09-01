6a287f2611a268aa112bc7bbeecccbde8c8c73c5
Florian Siegel <florian1siegel@googlemail.com>
2023-09-01 15:08:17 +0200

    replace image with image from private ECR

--- lib/aws-app-runner-cdk-stack.ts
+++ lib/aws-app-runner-cdk-stack.ts
@@ -1,17 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as apprunner from '@aws-cdk/aws-apprunner-alpha';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 
 export class AwsAppRunnerCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const appRunnerService = new apprunner.Service(this, "app-runner-service", {
-      source: apprunner.Source.fromEcrPublic({
-        imageIdentifier: "public.ecr.aws/aws-containers/hello-app-runner:latest",
+      source: apprunner.Source.fromEcr({
+        repository: ecr.Repository.fromRepositoryName(this, "image-repository", "manual-test"),
+        tagOrDigest: "latest",
         imageConfiguration: {
-            port: 8000
-        },
+          port: 80
+        }
       })
     })
 
