5de101569834ca6e14fc53490deac00211fde255
Florian Siegel <florian1siegel@googlemail.com>
2023-09-01 14:49:29 +0200

    Launch vercel example as app runner service

--- lib/aws-app-runner-cdk-stack.ts
+++ lib/aws-app-runner-cdk-stack.ts
@@ -1,10 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apprunner from '@aws-cdk/aws-apprunner-alpha';
 
 export class AwsAppRunnerCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const appRunnerService = new apprunner.Service(this, "app-runner-service", {
+      source: apprunner.Source.fromGitHub({
+        repositoryUrl: "https://github.com/vercel/next.js/tree/canary/examples/hello-world",
+        branch: "canary",
+        configurationSource: apprunner.ConfigurationSourceType.REPOSITORY,
+        connection: apprunner.GitHubConnection.fromConnectionArn("arn:aws:apprunner:eu-central-1:718517280342:connection/private-github/bb8596b912844bad8ebd7cb0f99143ac")
+      })
+    })
+
+    new cdk.CfnOutput(this, "app-runner-service-url", {
+      value: appRunnerService.serviceUrl
+    })
 
     // The code that defines your stack goes here
 
