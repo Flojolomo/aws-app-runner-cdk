96c04458adab882d118f1b72c045083cde8ae88f
Florian Siegel <florian1siegel@googlemail.com>
2023-09-04 09:11:38 +0200

    Provide ECR name as config

--- lib/aws-app-runner-cdk-stack.ts
+++ lib/aws-app-runner-cdk-stack.ts
@@ -3,14 +3,19 @@ import { Construct } from 'constructs';
 import * as apprunner from '@aws-cdk/aws-apprunner-alpha';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 
+type AwsAppRunnerCdkStackProps = cdk.StackProps & {
+  /** Name of the ECR repository providing the image to run. */
+  repositoryName: string
+}
+
 export class AwsAppRunnerCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: AwsAppRunnerCdkStackProps) {
     super(scope, id, props);
 
     const appRunnerService = new apprunner.Service(this, "app-runner-service", {
       serviceName: "AppRunnerExampleService",
       source: apprunner.Source.fromEcr({
-        repository: ecr.Repository.fromRepositoryName(this, "image-repository", "manual-test"),
+        repository: ecr.Repository.fromRepositoryName(this, "image-repository", props.repositoryName),
         tagOrDigest: "latest",
         imageConfiguration: {
           port: 80
